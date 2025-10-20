81a7d37e0aab8c5410931720829c97d7baf15c70
Jerónimo Infante <jinfante@buk.cl>
2025-10-19 22:54:33 -0300

    feat: add excel upload

---
+++ api/services/__init__.py
@@ -0,0 +1,38 @@
+"""
+Servicios para el procesamiento de archivos Excel
+"""
+
+from .excel_processor import ExcelProcessor
+from .processors import (
+    UserExcelProcessor,
+    PacienteExcelProcessor,
+    CamaExcelProcessor,
+    EpisodioExcelProcessor,
+    GestionExcelProcessor,
+)
+
+__all__ = [
+    'ExcelProcessor',
+    'UserExcelProcessor',
+    'PacienteExcelProcessor', 
+    'CamaExcelProcessor',
+    'EpisodioExcelProcessor',
+    'GestionExcelProcessor',
+]
+from .excel_processor import ExcelProcessor
+from .processors import (
+    UserExcelProcessor,
+    PacienteExcelProcessor,
+    CamaExcelProcessor,
+    EpisodioExcelProcessor,
+    GestionExcelProcessor,
+)
+
+__all__ = [
+    'ExcelProcessor',
+    'UserExcelProcessor',
+    'PacienteExcelProcessor', 
+    'CamaExcelProcessor',
+    'EpisodioExcelProcessor',
+    'GestionExcelProcessor',
+]
