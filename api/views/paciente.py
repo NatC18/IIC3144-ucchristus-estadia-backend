d230423822ee295e2eb9379bb79e59c4777aacae
Natalia Correal Posada <natalia.correal@uc.cl>
2025-10-19 23:52:51 -0300

    Revert "fix: linting"
    This reverts commit 9a0a48f444075a54e565c9171b0ae8263ba5e5c6.
    
    Error in branch commited

--- api/views/paciente.py
+++ api/views/paciente.py
@@ -9,13 +9,15 @@ from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from api.models import Episodio, Paciente
+from api.models import Paciente
 from api.serializers import (
-    EpisodioSerializer,
     PacienteCreateSerializer,
     PacienteListSerializer,
     PacienteSerializer,
 )
+
+from api.models import Episodio
+from api.serializers import EpisodioSerializer
 
 
 class PacienteViewSet(viewsets.ModelViewSet):
